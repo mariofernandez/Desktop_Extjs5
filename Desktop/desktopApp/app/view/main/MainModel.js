a250dff2b0b7959ce5628dc67d3b3849c53cd8cd
mariofernandez <siler_hack@hotmail.com>
2015-05-29 21:28:30 -0500

    [Mario Adrian ] Creacion de desktop en extjs5

---
+++ Desktop/desktopApp/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('desktop.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'desktop'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
---
+++ Desktop/desktopApp/build/temp/development/desktop/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /opt/lampp/htdocs/desarrolloEscritorio/Desktop/desktopApp/app.js
